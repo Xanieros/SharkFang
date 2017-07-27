3c802dda369ac683f0cb75039645fab2e85959dd
jon-moreno <jonathan.moreno324@gmail.com>
2017-07-27 16:02:25 -0400

    "Made login modal have transparent overlay (CSS); made table autogenerate (JS); styled table (Bootstrap); Made Navbar (HTML)"

--- SharkFangProj/WebContent/js/script.js
+++ SharkFangProj/WebContent/js/script.js
@@ -1,6 +1,6 @@
 var xSize=10;
 var ySize=10;
-var cells='';
+var rows='';
 
 function testFunction(){
 	window.alert("test called");
@@ -9,35 +9,33 @@ function testFunction(){
 function toggleLoginModal(){
 	
 //If the user authenticates
-  //document.getElementById("loginModal").setAttribute("class", "modal fade");
+  document.getElementById("loginModal").setAttribute("class", "modal fade");
   
 //If the user fails to authenticate
-  document.getElementById("errorMessage").innerHTML = 'Authentication Failed';
+  //document.getElementById("errorMessage").innerHTML = 'Authentication Failed';
   //$("#errorMessage").delay(5000).fadeOut();
 };
 
 function generatePlayerBoard(){
   
   //Table Headers
-  for(i=0; i<xSize; i++){
-      document.getElementById("playerBoard").innerHTML += '<th>'+i+'</th>';
+  for(i=1; i<xSize+1; i++){
+      document.getElementById("header").innerHTML += '<th>'+i+'</th>';
 
   }
   
-  //Unique Cells
-  for(i=0; i<xSize; i++){
-    cells+='<td><label>'+i+'</label></td>'
-    cells+='<td class="hit"><input type="radio" name="cell" value="'+i+'" disabled></td>';
-    console.log(cells);
+  //Make Rows
+  //Start with label
+  for(i=1; i<ySize+1; i++){
+    rows+='<tr><td><label>'+i+'</label></td>';
+    	//Append buttons
+        for(j=0; j<xSize; j++){
+        	rows+='<td class="bg-info"><input type="radio" name="cell" value="'+i+'"></td>';
+        }
+    //Close row tag once row made
+    rows+='</tr>';
   }
   
-  //Table Rows
-  for(i=0; i<ySize; i++){
-      document.getElementById("playerBoard").innerHTML += 
-        +'<tr>'+cells
-        //+'<td><label>'+i+'</label></td>'+cells
-        //+'<td class="hit"><input type="radio" name="cell" value="A1" disabled></td>'
-        +'</tr>';
-
-  }
+  document.getElementById("playerBoard").innerHTML += rows;
+  
 };
