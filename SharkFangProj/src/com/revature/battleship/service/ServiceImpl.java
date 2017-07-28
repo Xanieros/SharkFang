f4672d1adecf058fa6f8794c4c7778ad94291f92
yingvang4 <yingvang44@gmail.com>
2017-07-28 08:21:46 -0700

    Added middle-tier Initialize features

--- SharkFangProj/src/com/revature/battleship/service/ServiceImpl.java
+++ SharkFangProj/src/com/revature/battleship/service/ServiceImpl.java
@@ -5,16 +5,26 @@
 package com.revature.battleship.service;
 
 import com.revature.battleship.daos.PlayerDAO;
+import com.revature.battleship.gamelogic.GameDriver;
 import com.revature.battleship.pojos.Player;
 
 public class ServiceImpl implements Service {
 
 	// make Database object
 	PlayerDAO myPlayerDatabase = new PlayerDAO();
+	GameDriver myGameDriver;
 	
 	@Override
 	public Player login(String usernameEntered, String passwordEntered) {
 		return myPlayerDatabase.login(usernameEntered, passwordEntered);
 	}
 
+	@Override
+	public int initializeGame(int xSize, int ySize, int userPlayerID, int enemyPlayerID) {
+		int totalBoardSize = xSize * ySize;
+		myGameDriver = new GameDriver(totalBoardSize, totalBoardSize);
+		
+		int gameID = 1; // call db save game to db method, return game id, store that into the session in servlet
+		return gameID;
+	}
 }
